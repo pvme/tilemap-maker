12228bf3f7808e6467a4c6e2aa76183660ccee5f
x222 <113437965+rsnx222@users.noreply.github.com>
2024-06-11 20:45:13 +0100

    Remove convert to 64

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,13 +70,10 @@ $(function() {
             // Build cols
             for (let x = 0; x < mapWidthInTiles + 2; x++) {
                 var tileUrl = "https://runeapps.org/s3/map4/live/topdown-0/5/" + (mapX+x) + "-" + (mapY+y) + ".webp" //https://runeapps.org/s3/map4/live/topdown-{mej_plane}/5/{x}-{y}.webp
-               
-                toDataURL(tileUrl, function(tileEncodedAs64) {
-                    var $img = $("<img></img>").attr('src', tileEncodedAs64).width(imgWidth)
-                    var $imgBorder = $("<div class='map-square-border'></div>").width(imgWidth).height(imgWidth)
-                    $mapSquares.append($img)
-                    $mapSquaresBorders.append($imgBorder)
-                })
+                var $img = $("<img></img>").attr('src', tileUrl).width(imgWidth)
+                var $imgBorder = $("<div class='map-square-border'></div>").width(imgWidth).height(imgWidth)
+                $mapSquares.append($img)
+                $mapSquaresBorders.append($imgBorder)
             }
         }
 
@@ -117,17 +114,3 @@ $(function() {
         })
     })
 })
-
-function toDataURL(url, callback) {
-  var xhr = new XMLHttpRequest()
-  xhr.onload = function() {
-    var reader = new FileReader()
-    reader.onloadend = function() {
-      callback(reader.result)
-    }
-    reader.readAsDataURL(xhr.response)
-  }
-  xhr.open('GET', url)
-  xhr.responseType = 'blob'
-  xhr.send()
-}
